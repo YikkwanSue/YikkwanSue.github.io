3468b7c5ec31991ba11bdc3a29240ace12781fc6
YikkwanSue <liyikkwan.18@gmail.com>
2024-06-01 14:25:05 +1000

    updated

--- assignment3/script.js
+++ assignment3/script.js
@@ -1,9 +1,37 @@
-document.addEventListener("DOMContentLoaded", function() {
-  const balls = document.querySelectorAll('.ball');
 
-  balls.forEach(ball => {
-      ball.addEventListener('click', function() {
-          ball.style.backgroundColor = '#9e8880';
-      });
+
+// Staff
+
+document.addEventListener("DOMContentLoaded", function() {
+  // Get references to the images
+  const ownerImage = document.getElementById('ownerImage');
+  const chiefImage = document.getElementById('chiefImage');
+  const managerImage = document.getElementById('managerImage');
+
+  // Add event listeners for mouseover and mouseout to each image
+  ownerImage.addEventListener('mouseover', function() {
+    ownerImage.src = 'owner1.png';
+  });
+
+  ownerImage.addEventListener('mouseout', function() {
+    ownerImage.src = 'owner.png';
+  });
+
+  chiefImage.addEventListener('mouseover', function() {
+    chiefImage.src = 'chief1.png';
+  });
+
+  chiefImage.addEventListener('mouseout', function() {
+    chiefImage.src = 'chief.png';
+  });
+
+  managerImage.addEventListener('mouseover', function() {
+    managerImage.src = 'manager1.png';
+  });
+
+  managerImage.addEventListener('mouseout', function() {
+    managerImage.src = 'manager.png';
   });
 });
+
+
